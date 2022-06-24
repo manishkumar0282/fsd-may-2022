313c6d2d74273cf36d822242a3ef5c36d79e9acf
Manish Kumar <5203513+manish0282@users.noreply.github.com>
2022-06-24 22:55:46 +0530

    Notes for June 24

---
+++ notes/june24.js
@@ -0,0 +1,41 @@
+console.log("Basic Problem solving in Recursion");
+
+/*
+find out the number of ways in which 2*1 tile can be used to fill an
+area of 2*n
+*/
+
+function numberOfWays(n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+
+    return numberOfWays(n-1) + numberOfWays(n-2);
+}
+
+console.log(numberOfWays(4));
+console.log(numberOfWays(8));
+
+
+/*
+* Find the subsequences recursively
+*/
+
+function printSubsequenes(arr, i, result) {
+    if(i == arr.length) {
+        if(result.length > 0) {
+            console.log(result);
+        }
+    } else {
+        printSubsequenes(arr, i+1, result);
+        result.push(arr[i]);
+        printSubsequenes(arr, i+1, result);
+        result.pop();
+    }
+
+    return;
+}
+
+let arr = ["a","b","c"];
+let result = new Array();
+console.log(printSubsequenes(arr, 0, result));
